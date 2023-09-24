fdacf495be14d5542d9fdfe2dcce250bc3e864e0
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2023-09-24 23:48:23 +0300

    up

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ form.addEventListener('submit', (event) =>{
         return;
     }
         
-    result.innerHTML= `<span><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg></span>`
-    result.innerHTML = `<h1>Success ${email}</h1>`
+   
+    result.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" 
+    viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" 
+    stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none">
+    <circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g>
+    </svg> 
+    \r\n<h1>Success</h1> \r\n
+     <p>${email}</p>`
     
 });
 
